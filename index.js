b1cec1d8b3a8be9b72ac016c48451a16bc13bd9e
emmafewer <emmafewer20@gmail.com>
2021-01-11 16:11:36 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names){
+    let array = []
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return array;
+}
+
+function countDown(number){
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
