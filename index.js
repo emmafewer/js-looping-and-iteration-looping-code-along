d7b3c8b39af5dbfb3ee5ac8913181d2473ed20ac
emmafewer <emmafewer20@gmail.com>
2021-01-11 16:24:49 -0800

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function writeCards(names){
     let array = []
     for (let i = 0; i < names.length; i++) {
-        array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        array[i] = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
     }
     return array;
 }
